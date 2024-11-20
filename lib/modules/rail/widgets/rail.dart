e5309a3833a6e1f2684f54b534029712ceca4078
Ilia Liubinskii <ilia@gotolstoy.com>
2024-11-20 14:10:42 +0200

    Disable auto-scrolling, only play videos in view

--- lib/modules/rail/widgets/rail.dart
+++ lib/modules/rail/widgets/rail.dart
@@ -48,45 +48,11 @@ class _RailState extends State<Rail> {
     super.dispose();
   }
 
-  void _playNextVideo() {
-    setState(() {
-      _currentPlayingIndex = (_currentPlayingIndex + 1) %
-          widget.config.assets.length.clamp(0, maxVisibleItems);
-    });
-    _scrollToCurrentVideo();
-  }
-
-  void _scrollToCurrentVideo() {
-    if (_scrollController.hasClients) {
-      final railWidth =
-          context.size?.width ?? MediaQuery.of(context).size.width;
-      final itemWidth = widget.options.itemWidth + widget.options.itemGap;
-      final visibleItemCount =
-          widget.config.assets.length.clamp(0, maxVisibleItems);
-      final remainingItems = (visibleItemCount + 1) - _currentPlayingIndex;
-      final remainingWidth = remainingItems * itemWidth;
-
-      if (remainingWidth > railWidth) {
-        final targetOffset = _currentPlayingIndex * itemWidth;
-        final maxOffset =
-            (visibleItemCount * itemWidth) - railWidth - widget.options.itemGap;
-
-        if (maxOffset > 0.0) {
-          final clampedOffset = targetOffset.clamp(0.0, maxOffset);
-
-          _scrollController.animateTo(
-            clampedOffset,
-            duration: const Duration(milliseconds: 300),
-            curve: Curves.easeInOut,
-          );
-        }
-      }
-    }
+  int get visibleItemCount {
+    return widget.config.assets.length.clamp(0, maxVisibleItems);
   }
 
   bool _shouldPreload(int index) {
-    final visibleItemCount =
-        widget.config.assets.length.clamp(0, maxVisibleItems);
     if (_currentPlayingIndex == 0) {
       // if first playing: preload first and second items
       return index == 0 || index == 1;
@@ -102,70 +68,125 @@ class _RailState extends State<Rail> {
     }
   }
 
+  bool __isVideoFullyInView(int index) {
+    final railWidth = context.size?.width ?? MediaQuery.of(context).size.width;
+    final itemWidth = widget.options.itemWidth + widget.options.itemGap;
+    final currentScrollOffset = _scrollController.position.pixels;
+
+    final itemStartOffset = index * itemWidth;
+    final itemEndOffset = itemStartOffset + widget.options.itemWidth;
+
+    return itemStartOffset >= currentScrollOffset &&
+        itemEndOffset <= currentScrollOffset + railWidth;
+  }
+
+  int? _getFirstVideoFullyInView() {
+    for (var i = 0; i < visibleItemCount; i++) {
+      if (__isVideoFullyInView(i)) {
+        return i;
+      }
+    }
+
+    return null;
+  }
+
+  void _playNextVideoIfInView() {
+    final nextIndex = _currentPlayingIndex + 1;
+
+    if (nextIndex < visibleItemCount && __isVideoFullyInView(nextIndex)) {
+      setState(() {
+        _currentPlayingIndex = nextIndex;
+      });
+    }
+  }
+
+  void _onScrollEnd() {
+    // if current video is still in view, continue playing
+    if (__isVideoFullyInView(_currentPlayingIndex)) {
+      return;
+    }
+
+    final firstVideoInView = _getFirstVideoFullyInView();
+
+    if (firstVideoInView != null) {
+      setState(() {
+        _currentPlayingIndex = firstVideoInView;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return VisibilityDetector(
-      key: Key('rail-${widget.config.publishId}'),
-      onVisibilityChanged: (visibilityInfo) {
-        if (visibilityInfo.visibleFraction > 0.5 && !_hasBeenVisible) {
-          _hasBeenVisible = true;
-          _analytics.sendEmbedView(widget.config);
+    return NotificationListener<ScrollNotification>(
+      onNotification: (scrollNotification) {
+        if (scrollNotification is ScrollEndNotification) {
+          _onScrollEnd();
         }
+
+        return true;
       },
-      child: SizedBox(
-        height: widget.options.itemHeight,
-        child: ListView.builder(
-          controller: _scrollController,
-          scrollDirection: Axis.horizontal,
-          itemCount: widget.config.assets.length.clamp(0, maxVisibleItems),
-          itemBuilder: (context, index) {
-            final asset = widget.config.assets[index];
-            final isFirstItem = index == 0;
-            final isLastItem = index ==
-                widget.config.assets.length.clamp(0, maxVisibleItems) - 1;
+      child: VisibilityDetector(
+        key: Key('rail-${widget.config.publishId}'),
+        onVisibilityChanged: (visibilityInfo) {
+          if (visibilityInfo.visibleFraction > 0.5 && !_hasBeenVisible) {
+            _hasBeenVisible = true;
+            _analytics.sendEmbedView(widget.config);
+          }
+        },
+        child: SizedBox(
+          height: widget.options.itemHeight,
+          child: ListView.builder(
+            controller: _scrollController,
+            scrollDirection: Axis.horizontal,
+            itemCount: visibleItemCount,
+            itemBuilder: (context, index) {
+              final asset = widget.config.assets[index];
+              final isFirstItem = index == 0;
+              final isLastItem = index == visibleItemCount - 1;
 
-            return Padding(
-              padding: EdgeInsets.only(
-                left: isFirstItem
-                    ? widget.options.xPadding
-                    : widget.options.itemGap / 2,
-                right: isLastItem
-                    ? widget.options.xPadding
-                    : widget.options.itemGap / 2,
-              ),
-              child: RailAsset(
-                asset: asset,
-                config: widget.config,
-                onTap: () {
-                  widget.onAssetClick?.call(asset);
-                  _analytics
-                      .sendVideoClicked(widget.config, {'videoId': asset.id});
-                },
-                onPlayClick: () {
-                  widget.onPlayClick?.call(asset);
-                  _analytics
-                      .sendVideoWatched(widget.config, {'videoId': asset.id});
-                },
-                onVideoEnded: (asset) {
-                  if (index != _currentPlayingIndex) {
-                    return;
-                  }
-
-                  _playNextVideo();
-                },
-                width: widget.options.itemWidth,
-                height: widget.options.itemHeight,
-                options: AssetViewOptions(
-                  isPlaying: index == _currentPlayingIndex,
-                  isMuted: true,
-                  shouldLoop: false,
-                  imageFit: BoxFit.cover,
-                  playMode: AssetViewOptionsPlayMode.preview,
+              return Padding(
+                padding: EdgeInsets.only(
+                  left: isFirstItem
+                      ? widget.options.xPadding
+                      : widget.options.itemGap / 2,
+                  right: isLastItem
+                      ? widget.options.xPadding
+                      : widget.options.itemGap / 2,
                 ),
-                preload: _shouldPreload(index),
-              ),
-            );
-          },
+                child: RailAsset(
+                  asset: asset,
+                  config: widget.config,
+                  onTap: () {
+                    widget.onAssetClick?.call(asset);
+                    _analytics
+                        .sendVideoClicked(widget.config, {'videoId': asset.id});
+                  },
+                  onPlayClick: () {
+                    widget.onPlayClick?.call(asset);
+                    _analytics
+                        .sendVideoWatched(widget.config, {'videoId': asset.id});
+                  },
+                  onVideoEnded: (asset) {
+                    if (index != _currentPlayingIndex) {
+                      return;
+                    }
+
+                    _playNextVideoIfInView();
+                  },
+                  width: widget.options.itemWidth,
+                  height: widget.options.itemHeight,
+                  options: AssetViewOptions(
+                    isPlaying: index == _currentPlayingIndex,
+                    isMuted: true,
+                    shouldLoop: false,
+                    imageFit: BoxFit.cover,
+                    playMode: AssetViewOptionsPlayMode.preview,
+                  ),
+                  preload: _shouldPreload(index),
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
