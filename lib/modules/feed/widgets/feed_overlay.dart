13e80e422522365fdab18b3ab7a1bcf815f6f2c3
Ilia Liubinskii <ilia@gotolstoy.com>
2024-11-20 17:45:08 +0200

    Add more button

--- lib/modules/feed/widgets/feed_overlay.dart
+++ lib/modules/feed/widgets/feed_overlay.dart
@@ -57,6 +57,35 @@ class FeedAssetOverlay extends StatelessWidget {
         Positioned(
           left: 0,
           right: 0,
+          top: 0,
+          child: Column(mainAxisSize: MainAxisSize.min, children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 8.0, vertical: 60.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF222222).withAlpha(204),
+                      shape: BoxShape.circle,
+                    ),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.more_vert,
+                        color: Colors.white,
+                      ),
+                      onPressed: onMuteUnmute,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ]),
+        ),
+        Positioned(
+          left: 0,
+          right: 0,
           bottom: 0,
           child: Column(
             mainAxisSize: MainAxisSize.min,
