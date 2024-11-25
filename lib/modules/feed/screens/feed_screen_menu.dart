1e06e7abf4b366a68ed06640e7709417406a1073
Ilia Liubinskii <ilia@gotolstoy.com>
2024-11-25 16:27:09 +0200

    Add confirmation on submitting report

--- lib/modules/feed/screens/feed_screen_menu.dart
+++ lib/modules/feed/screens/feed_screen_menu.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'feed_screen_main_menu.dart';
 import 'feed_screen_report_menu.dart';
+import 'feed_screen_report_submitted_menu.dart';
 
 class FeedScreenMenu extends StatefulWidget {
   final Future<void> Function({required String id, required String title})
@@ -25,35 +26,43 @@ class FeedScreenMenu extends StatefulWidget {
   State<FeedScreenMenu> createState() => _FeedScreenMenuState();
 }
 
+enum Screen { main, report, reportSubmitted }
+
 class _FeedScreenMenuState extends State<FeedScreenMenu> {
   final _mainMenuKey = UniqueKey();
   final _reportMenuKey = UniqueKey();
 
-  bool _showReportMenu = false;
+  var _selectedScreen = Screen.main;
 
   @override
   Widget build(BuildContext context) {
     return AnimatedSize(
-      duration: const Duration(milliseconds: 300),
-      curve: Curves.easeInOut,
-      child: _showReportMenu
-          ? FeedScreenReportMenu(
-              key: _mainMenuKey,
-              onCancel: () => setState(() => _showReportMenu = false),
-              onReport: ({required String id, required String title}) async {
-                await widget.onReport(id: id, title: title);
-                if (mounted) Navigator.pop(this.context);
-              },
-            )
-          : FeedScreenMainMenu(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+        child: switch (_selectedScreen) {
+          Screen.main => FeedScreenMainMenu(
               key: _reportMenuKey,
-              onReport: () => setState(() => _showReportMenu = true),
+              onReport: () => setState(() => _selectedScreen = Screen.report),
               hideReportButton: widget.hideReportButton,
               hideShareButton: widget.hideShareButton,
               customMenuTitle: widget.customMenuTitle,
               customMenuSubtitle: widget.customMenuSubtitle,
               customMenuLogoUrl: widget.customMenuLogoUrl,
             ),
-    );
+          Screen.report => FeedScreenReportMenu(
+              key: _mainMenuKey,
+              onCancel: () => setState(() => _selectedScreen = Screen.main),
+              onReport: ({required String id, required String title}) async {
+                await widget.onReport(id: id, title: title);
+
+                if (mounted) {
+                  setState(() => _selectedScreen = Screen.reportSubmitted);
+                }
+              },
+            ),
+          Screen.reportSubmitted => FeedScreenReportSubmittedMenu(
+              onClose: () => Navigator.pop(this.context),
+            ),
+        });
   }
 }
