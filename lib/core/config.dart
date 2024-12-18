67939e2bd92e7398b8ab6c319778fe4e0a27afc1
Ilia Liubinskii <ilia@gotolstoy.com>
2024-12-18 13:10:36 +0200

    Refactor

--- lib/core/config.dart
+++ lib/core/config.dart
@@ -1,17 +1,21 @@
 import "package:tolstoy_flutter_sdk/utils/debug_print.dart";
 
 class AppConfig {
+  // Options
+
   static const _debugLevel = DebugLevel.info;
 
   static const _mobileAppEndpoints = true;
 
   static const _prod = true;
 
+  // Local variables
+
   static const _devPart = _prod ? "" : "dev-";
 
   static const _mobileAppPart = _mobileAppEndpoints ? "/mobile-app" : "";
 
-  static const debugLevel = _debugLevel;
+  // Configuration
 
   static const analyticsEndpointUrl =
       "https://${_devPart}api.gotolstoy.com/events/event";
@@ -21,6 +25,8 @@ class AppConfig {
 
   static const configEndpointCacheUrl =
       "https://${_devPart}cf-apilb.gotolstoy.com/settings$_mobileAppPart/player/by-publish-id";
+
+  static const debugLevel = _debugLevel;
 
   static const productsEndpointUrl =
       "https://${_devPart}apilb.gotolstoy.com/products/actions/v2$_mobileAppPart/get-by-vod-asset-ids";
